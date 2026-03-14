cad27c56e2a418ed61b82c383cd5e08351210644
MellwynA <melvinprofessionalkva@gmail.com>
2026-03-14 22:05:46 +0530

    Boiler_plate

---
+++ src/test/java/com/example/common_library_service/CommonLibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.common_library_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonLibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
